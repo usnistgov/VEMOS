a629c06488c3e8356a33c52b2f8fbb9a745371b5
gdogan <gunay.dogan@nist.gov>
2019-08-20 13:49:34 -0400

    Added files

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 09:18:15 2019
+
+@author: Eve Fleisig
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+     name='VEMOS',  
+     version='1.1.1',
+     author="Eve Fleisig",
+     author_email="efleisig@princeton.edu",
+     description="Visual Explorer for Metrics of Similarity",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="",
+     packages=setuptools.find_packages("."),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: BSD License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=[
+	    "PyQt5", 
+         "numpy>=1.15", 
+         "scipy>=1.2", 
+         "matplotlib>=1.4.3", 
+         "scikit-learn>=0.20", 
+         "scikit-image>=0.15"]
+ )
